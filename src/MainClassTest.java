057c40925a5951e278293492e525cfdaf5788bd5
HomeName <afonin_av@list.ru>
2019-12-28 15:32:15 +0300

    Test_2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,18 +3,30 @@ import org.junit.Test;
 
 public class MainClassTest extends MainClass
 {
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int a = this.getClassNumber();
+        if (a > 45)
+        {
+            System.out.println("Number == " + a);
+
+        } else {
+            Assert.fail("Number <= 45");
+        }
+    }
+
     @Test
     public void testGetLocalNumber()
     {
         int a = this.getLocalNumber();
         //System.out.println(this.getLocalNumber());
-
         if (a != 14)
         {
             Assert.fail("Number != 14");
         } else {
             System.out.println("Number == 14");
         }
-
     }
 }
