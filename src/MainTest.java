304aedbe4a54cd12610867e3c98e48530197bc45
HomeName <afonin_av@list.ru>
2019-12-28 12:23:02 +0300

    Beginning

---
+++ src/MainTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class MainTest extends CoreTestCase
+{
+    MathHelper Math = new MathHelper();
+
+
+    @Test
+    public void myFirstTest() {
+
+        int a = Math.multiply(5);
+        System.out.println(a);
+
+        int b = Math.multiply(10, 15);
+        System.out.println(b);
+    }
+
+
+}
---
+++ src/MathHelper.java
@@ -0,0 +1,13 @@
+public class MathHelper
+{
+    public int multiply(int number)
+    {
+        return number * 2;
+    }
+
+
+    public int multiply(int number, int multiplier)
+    {
+        return number * multiplier;
+    }
+}
