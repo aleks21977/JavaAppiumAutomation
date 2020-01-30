369bd4dc2d36b00a0fe9f188c9cb0c3b8a32db37
Home Windows <afonin_av@list.ru>
2020-01-30 14:52:41 +0300

    Temp19

---
+++ src/tests/iOS/GetStartedTest.java
@@ -0,0 +1,26 @@
+package tests.iOS;
+
+import lib.iOSTestCase;
+import lib.ui.WelcomePageObject;
+import org.junit.Test;
+
+public class GetStartedTest extends iOSTestCase {
+
+    @Test
+    public void testPassThroughWeWelcome()
+    {
+        WelcomePageObject WelcomePage = new WelcomePageObject(driver);
+
+        WelcomePage.waitForLearnMoreLink();
+        WelcomePage.clickNextButton();
+
+        WelcomePage.waitForNewWayToExploreText();
+        WelcomePage.clickNextButton();
+
+        WelcomePage.waitForAddForEditPreferredLangText();
+        WelcomePage.clickNextButton();
+
+        WelcomePage.waitForLearnMoreAboutDataCollectedText();
+        WelcomePage.clickGetStartedButton();
+    }
+}
